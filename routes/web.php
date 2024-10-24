35fa1790fcd3acf6061107608d132219366b0df6
Devnull <devnull@arch.localdomain>
2024-10-24 16:46:17 +0300

    Uygulama dockerize edildi.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\LocationController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/locations');
+});
+
 Route::prefix('locations')->middleware('throttle:30,1')->group(function () {
     Route::get('/', [LocationController::class, 'index'])->name('locations.index'); // Konumları Listele
     Route::get('/create', [LocationController::class, 'create'])->name('locations.create'); // Yeni Konum Ekleme Sayfası
